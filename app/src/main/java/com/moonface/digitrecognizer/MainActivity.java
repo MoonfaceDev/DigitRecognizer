e677a54e88af4a60d9a54d339b7e238211f946f3
elaic <elaicorem1@gmail.com>
2020-01-10 13:01:09 +0200

    changes

--- app/src/main/java/com/moonface/digitrecognizer/MainActivity.java
+++ app/src/main/java/com/moonface/digitrecognizer/MainActivity.java
@@ -1,14 +1,56 @@
 package com.moonface.digitrecognizer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_CAMERA = 0;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void launchCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_CAMERA);
+        }
+    }
+
+    private void loadImage(Bitmap bitmap){
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            //load bitmap from data intent
+            Bundle extras = data.getExtras();
+            assert extras != null;
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //convert the bitmap into byte array
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            assert imageBitmap != null;
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            imageBitmap.recycle();
+            //TODO: remove
+            for(byte b : byteArray){
+                Log.d("BYTE ARRAY", String.valueOf(b));
+            }
+            //loadImage(imageBitmap);
+        }
     }
 }
