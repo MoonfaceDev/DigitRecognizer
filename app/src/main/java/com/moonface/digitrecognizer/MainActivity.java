2a7f99473f0b6501c462368865541cab45eb7470
elaic <elaicorem1@gmail.com>
2020-01-11 13:58:19 +0200

    changes

--- app/src/main/java/com/moonface/digitrecognizer/MainActivity.java
+++ app/src/main/java/com/moonface/digitrecognizer/MainActivity.java
@@ -2,6 +2,7 @@ package com.moonface.digitrecognizer;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -11,10 +12,10 @@ import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,13 +24,14 @@ import androidx.core.content.ContextCompat;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,8 +40,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CAMERA = 0;
     private static final int REQUEST_PERMISSION = 1;
+    private static final DecimalFormat decimal = new DecimalFormat("0.00");
     private ImageView imageView;
     private Bitmap imageBitmap;
+    private double[] stats;
+    private View parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FloatingActionButton cameraButton = findViewById(R.id.camera_button);
         MaterialButton scanButton = findViewById(R.id.scan_button);
+        MaterialButton dataButton = findViewById(R.id.data_button);
         imageView = findViewById(R.id.digit_image);
+        parent = findViewById(R.id.parent);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +68,24 @@ public class MainActivity extends AppCompatActivity {
                 scanImage();
             }
         });
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchDataDialog();
+            }
+        });
+    }
+
+    private void launchDataDialog(){
+        if(stats != null) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            StringBuilder dataString = new StringBuilder();
+            for (int i = 0; i < stats.length; i++) {
+                dataString.append(i).append(":  ").append(decimal.format(stats[i] * 100)).append("%\n");
+            }
+            dialogBuilder.setMessage(dataString.toString());
+            dialogBuilder.create().show();
+        }
     }
 
     //launches the camera intent
@@ -107,14 +132,16 @@ public class MainActivity extends AppCompatActivity {
         bitmap.getPixels(matrix, 0, bitmap.getWidth(),0,0,bitmap.getWidth(),bitmap.getHeight());
         double[] d_matrix = new double[matrix.length];
         for(int i=0; i<matrix.length; i++){
-            d_matrix[i] = matrix[i];
+            d_matrix[i] = 0xFF-(matrix[i]&0xFF);
+            if(d_matrix[i] < 0x80){
+                d_matrix[i] = 0;
+            }
         }
         return d_matrix;
     }
 
     private @NonNull double[][] dot(double[][] a, double[][] b){
         if(a[0].length != b.length){
-            Log.d("MOONFACE", "a: "+a.length+" ?= b: "+b[0].length);
             return new double[0][0];
         }
         int height = a.length;
@@ -128,14 +155,6 @@ public class MainActivity extends AppCompatActivity {
         return out;
     }
 
-    private double[][] make2D(double[] a){
-        double[][] b = new double[a.length][];
-        for(int i=0; i<a.length; i++){
-            b[i] = new double[]{a[i]};
-        }
-        return b;
-    }
-
     private double dotCell(double[][] a, double[][] b, int i, int j) {
         double c = 0;
         for (int k = 0; k < b.length; k++) {
@@ -145,8 +164,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private double[] sigmoid(double[] a){
-        double[] b = a.clone();
-        for(int i=0; i<=a.length; i++){
+        double[] b = new double[a.length];
+        for(int i=0; i<a.length; i++){
             b[i] = 1/(1+Math.exp(-a[i]));
         }
         return b;
@@ -180,14 +199,20 @@ public class MainActivity extends AppCompatActivity {
     //scans the digit image and displays the result
     private void scanImage(){
         if(imageBitmap != null) {
-            double[] matrix = toMatrix(toGrayscale(getResizedBitmap(imageBitmap)));
+            double[] matrix = toMatrix(imageBitmap);
             double[][][] weights = importWeights();
             double[] biases = importBiases();
             assert weights != null;
             assert biases != null;
             double[] hidden = sigmoid(add(dot(new double[][]{matrix}, weights[0])[0], biases[0]));
             double[] output = sigmoid(add(dot(new double[][]{hidden}, weights[1])[0], biases[1]));
+            stats = output.clone();
             int result = maxIndex(output);
+            StringBuilder matrixBuilder = new StringBuilder();
+            for(double i : matrix){
+                matrixBuilder.append(i);
+                matrixBuilder.append(",");
+            }
             TextView resultView = findViewById(R.id.result_text);
             resultView.setText(String.valueOf(result));
         }
@@ -264,23 +289,24 @@ public class MainActivity extends AppCompatActivity {
             //load bitmap from data intent
             Bundle extras = data.getExtras();
             assert extras != null;
-            imageBitmap = (Bitmap) extras.get("data");
-            assert imageBitmap != null;
+            Bitmap temp = (Bitmap) extras.get("data");
+            assert temp != null;
+            imageBitmap = toGrayscale(getResizedBitmap(temp));
             //load image into imageView
             loadImage(imageBitmap);
         } else {
-            //TODO: handle error
+            Snackbar.make(parent, "Error occurred. Cannot load image", Snackbar.LENGTH_SHORT).show();
         }
     }
 
     //launch camera after got permission
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 launchCamera();
             } else {
-                //TODO: handle error
+                Snackbar.make(parent, "Permission denied. Please try again.", Snackbar.LENGTH_SHORT).show();
             }
         }
     }
